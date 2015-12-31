42499762266fc2d83ac9aa02797fecca973ade3c
ekhumalo <ekhumalo@erics-mbp.mobile.asu.edu>
2015-12-31 12:31:36 -0700

    Email

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'ekhumalo@berkeley.edu';//replace with your email
+$to   		= 'ekhumalo@usapglobal.org';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
