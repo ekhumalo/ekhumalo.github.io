80166ebb50c5dd7dd1c8dfee6eef089a6a3c97cc
ekhumalo <ekhumalo@berkeley.edu>
2016-01-31 18:05:34 -0800

    change

--- nomaqhawe/main.js
+++ nomaqhawe/main.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Nomaqhawe!";
+var myName = "NOMA!";
 var letterColors = [red, orange, green, blue, purple, black]
 if(true) {
     bubbleShape = "circle";
