8f0bf3090ee52a21f310ea6f737e77e93b7e17bc
ekhumalo <ekhumalo@berkeley.edu>
2016-02-20 21:35:09 -0800

    Additions
    For birthday corrections

---
+++ x/main.js
@@ -0,0 +1,19 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "ALUGA!";
+var letterColors = [red, orange, green, blue, purple, black]
+if(true) {
+    bubbleShape = "circle";
+}
+else {
+    bubbleShape = "square";
+}
+
+
+
+drawName(myName, letterColors);
+bounceBubbles()
