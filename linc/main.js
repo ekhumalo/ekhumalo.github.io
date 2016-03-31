b37f649ac6d86fb2cceaac159794b914d08954ef
ekhumalo <ekhumalo@berkeley.edu>
2016-03-31 13:05:01 -0700

    line

--- linc/main.js
+++ linc/main.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "ALUGA!";
+var myName = "LINC!";
 var letterColors = [red, orange, green, blue, purple, black]
 if(true) {
     bubbleShape = "circle";
