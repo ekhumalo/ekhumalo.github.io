183cec9733a74594d0a5be26bf1c8cb6a5dc6285
Eric Khumalo <ekhumalo@berkeley.edu>
2020-09-16 06:17:09 -0700

    New site for cs7 fall 2020

--- cs7/lab/lab1/lab1.py
+++ cs7/lab/lab1/lab1.py
@@ -1,13 +1,13 @@
 #Fill in your name and surname
-name = ""
-surname = ""
+name = "lambda"
+surname = "recursion"
 assignment = "lab1"
 
-def twenty_nineteen():
-    """Come up with the most creative expression that evaluates to 2019,
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_nineteen()
-    2019
+    >>> twenty_twenty()
+    2020
     """
-    return ______
+    return ________
---
+++ old_cs7/lab/lab3/lab3/lab3.py
@@ -0,0 +1,20 @@
+"""Lab 2: Lambda Expressions and Higher Order Functions"""
+
+#Fill in your name and surname
+name = ""
+surname = ""
+assignment = "lab3"
+
+# Lambda Functions
+
+def lambda_curry2(func):
+    """
+    Returns a Curried version of a two-argument function FUNC.
+    >>> from operator import add
+    >>> curried_add = lambda_curry2(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    """
+    "*** YOUR CODE HERE ***"
+    return ______
